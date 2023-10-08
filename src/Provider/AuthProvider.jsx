362fc34830aad887e8338fc8bf51f5466a296cf6
EnayetHossain <enayethossasin135@gmail.com>
2023-10-08 16:42:21 +0600

    auth provider

---
+++ src/Provider/AuthProvider.jsx
@@ -0,0 +1,77 @@
+/* eslint-disable react/prop-types */
+import {
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import app from "../Firebase/Firebase.config";
+
+export const AuthContext = createContext(null);
+const auth = getAuth(app);
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const googleProvider = new GoogleAuthProvider();
+
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const signInUser = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const logOutUser = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  const googleSignIn = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  const updateUser = (name, photo, phone) => {
+    setLoading(true);
+    return updateProfile(auth.currentUser, {
+      displayName: name,
+      photoURL: phone,
+      phone: phone,
+    });
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const authInfo = {
+    user,
+    loading,
+    createUser,
+    signInUser,
+    logOutUser,
+    googleSignIn,
+    updateUser,
+  };
+
+  return (
+    <>
+      <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+    </>
+  );
+};
+
+export default AuthProvider;
